de6175809d9b3d6641ffbab2cf67900bf032e981
acoustik <ajay@acoustik.org>
2014-01-17 15:38:03 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OlDailyBitcoin::Application.config.secret_token = '963a837d02d62c16f61e25b19311396f54c2c47dc03832190477e63837be299c080fc269dbd2271d622ca1f2c394fcfc7ceb07639d7e896c51470e6f3b8dbfec'
