7e5ca1db2dbd71ece80a23a09dbd3ca270555023
acoustik <ajay@acoustik.org>
2014-01-19 12:32:34 -0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 OlDailyBitcoin::Application.routes.draw do
   match '/' => 'register#index', :via => :get
-
-  resources :super_market, :sms
+  match 'register/welcome' => 'register#welcome', :via => :get
+  
+  resources :super_market, :sms, :register
 
 end
