79a18d6393322cd39352da31db728086498cea87
acoustik <ajay@acoustik.org>
2014-01-20 22:29:31 -0500

    .

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,5 @@ module OlDailyBitcoin
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.assets.compile = true
   end
 end
