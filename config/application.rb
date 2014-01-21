a5bc96597dfb9ec1334ee4f9f3ec4adc34ba419b
acoustik <ajay@acoustik.org>
2014-01-20 22:19:06 -0500

    still trying to get bootstrap to work

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module OlDailyBitcoin
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.compile = true
   end
 end
