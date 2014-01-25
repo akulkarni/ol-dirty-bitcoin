8b4d6ae2965f206f1dc6c6609335fdc6f5126870
Ajay Kulkarni <ajay@acoustik.org>
2014-01-24 22:47:53 -0500

    .

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -42,8 +42,8 @@ end
 
 private
 
-WELCOME_MESSAGE = "Coin Rules Everything Around Me.\n\nWelcome to ODBTC! Reply with STOP to unsubscribe at any time.\n\nfollow us: http://twitter.com/odbtc"
-HELP_MESSAGE = "Shimmy shimmy ya\n\n\"p\" or \"price\" for latest prices \n\"STOP\" to unsubscribe\n\nfollow us: http://twitter.com/odbtc"
+WELCOME_MESSAGE = "Coin Rules Everything Around Me.\n\nWelcome to ODBTC! Reply with STOP to unsubscribe at any time.\n\nhttp://twitter.com/odbtc"
+HELP_MESSAGE = "Shimmy shimmy ya\n\n\"p\" or \"price\" for latest prices \n\"STOP\" to unsubscribe\n\nhttp://twitter.com/odbtc"
 
 def register_user(phone_number)
   response = "Something bad happened."
